3a0f9967b229fa3af60fe2c93712e22a58d94ef0
Germán Mauricio Muñoz <mauricio@pager.com>
2019-06-19 08:51:29 -0500

    feat: implement first version of event publishing interface (#2)

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 'use strict';
 
-module.exports = () => {
 
-};
+class Client {
+
+    constructor(transport) {
+
+        this.transport = transport;
+    }
+
+    publish(stream, eventType, data) {
+
+        return this.transport.publish(stream, eventType, data);
+    }
+}
+
+module.exports = Client;
--- test/index.js
+++ test/index.js
@@ -3,6 +3,8 @@
 const Code = require('@hapi/code');
 const Lab = require('@hapi/lab');
 
+const Client = require('../index');
+
 const lab = exports.lab = Lab.script();
 const { it, describe } = lab;
 const expect = Code.expect;
@@ -11,8 +13,26 @@ describe('event sourcing client', () => {
 
     it('should be exported as function', () => {
 
-        const Client = require('../index');
-
         expect(typeof Client).to.equal('function');
+    });
+
+    it('should delegate publishing to configured transport', () => {
+
+        const stream = 'chats';
+        const eventType = 'chatCreated';
+        const data = { key: 'value' };
+
+        const transport = {
+            publish: (_stream, _eventType, _data) => ({
+                stream: _stream,
+                eventType: _eventType,
+                data: _data
+            })
+        };
+        const client = new Client(transport);
+
+        expect(client.publish(stream, eventType, data)).to.equal({
+            stream, eventType, data
+        });
     });
 });
