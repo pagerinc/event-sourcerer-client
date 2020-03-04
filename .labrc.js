633e983b91ed896f412f0ce97eaea5b9f78778c0
Gaston Festari <gaston.festari@pager.com>
2020-03-04 19:31:15 -0300

    fix(deps): update dependency uuid to v7 (#118)

--- .labrc.js
+++ .labrc.js
@@ -2,7 +2,7 @@ module.exports = {
     coverage: true,
     threshold: 100,
     lint: true,
-    globals: '__extends,__assign,__rest,__decorate,__param,__metadata,__awaiter,__generator,__exportStar,__values,__read,__spread,__await,__asyncGenerator,__asyncDelegator,__asyncValues,__makeTemplateObject,__importStar,__importDefault,__SENTRY__,__spreadArrays',
+    globals: '__extends,__assign,__rest,__decorate,__param,__metadata,__awaiter,__generator,__exportStar,__values,__read,__spread,__await,__asyncGenerator,__asyncDelegator,__asyncValues,__makeTemplateObject,__importStar,__importDefault,__SENTRY__,__spreadArrays,__classPrivateFieldGet,__classPrivateFieldSet',
     assert: '@hapi/code',
     verbose: true
 };
